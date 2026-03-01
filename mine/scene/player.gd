6070b6709744a3fd9ec8cf96f6fa173efddc35d6
AndreycoImaschi56 <pirroni.paolo10@gmail.com>
2026-03-01 22:32:58 +0100

    2

--- mine/scene/player.gd
+++ mine/scene/player.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 100
 
+var last_direction: Vector2 = Vector2.RIGHT
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
+	
 	process_movement()
+	process_animation()
 	move_and_slide()
 
 func process_movement() -> void:
@@ -14,6 +19,23 @@ func process_movement() -> void:
 	
 	if direction != Vector2.ZERO:
 		velocity = SPEED * direction
+		last_direction = direction
 	else:
 		velocity = Vector2.ZERO
-		
+
+func process_animation() -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", last_direction)
+	else:
+		play_animation("idle", last_direction)
+
+
+
+func play_animation (prefix: String, dir : Vector2) -> void:
+	if dir.x != 0:
+		sprite.flip_h = dir.x < 0 
+		sprite.play(prefix + "_right")
+	elif dir.y < 0:
+		sprite.play (prefix + "_up")
+	elif dir.y > 0:
+		sprite.play (prefix + "_down")
