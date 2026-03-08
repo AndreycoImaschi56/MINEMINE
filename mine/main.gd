e9f7ee16136c87fae594ab212a7b6c4b5c0f304d
AndreycoImaschi56 <pirroni.paolo10@gmail.com>
2026-03-08 23:56:23 +0100

    8

--- mine/main.gd
+++ mine/main.gd
@@ -1,16 +1,30 @@
 extends Node2D
 
 var level: int = 1
-var current_level: Node = null
+var current_level = null
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_level = get_node("levelroot")
-	
-	var uscita = current_level.get_node_or_null("uscita")
-	if uscita:
-		uscita.body_entered.connect(_on_uscita_body_entered)
+	_load_level(level)
+
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		level +=1
+		call_deferred("_load_level", level)
+
+
+func _load_level(level_number:int) -> void:
+	if current_level:
+		current_level.queue_free()
 		
-func _on_uscita_body_entered(body: Node2D) -> void:
+	var level_path = "res://scene/levels/level_%s.tscn" % level_number
+	current_level = load(level_path).instantiate()
+	add_child(current_level)
+	current_level.name = "levelroot"
+	_setup_level(current_level)
 	
+func _setup_level(level_root: Node) -> void:
+	var exit = level_root.get_node_or_null("uscita")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
